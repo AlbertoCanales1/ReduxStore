ae9da24cc1a4a5e61dba6b8ee5d093df9de8d639
AlbertoCanales1 <albertocanales1@outlook.com>
2021-10-08 19:13:45 -0500

    added some reducer files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combinedReducers } from redux;
 import home from './home_reducer';
-import shop from './shop_reducer'
+import shop from './shop_reducer.js'
 
 const rootReducer = combinedReducers ({
     home, shop
