abf748bff8104f6966e7d18b3304a2671016dc23
AlbertoCanales1 <albertocanales1@outlook.com>
2021-10-08 23:34:58 -0500

    fixed app issues after refactor

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import OrderHistory from './pages/OrderHistory';
 import store from './utils/store'
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
